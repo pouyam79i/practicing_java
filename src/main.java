5a5ab2b8209e86cab8816e16835f65560eaa1629
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-03-01 11:19:40 +0300

    tiketMachin created

---
+++ src/main.java
@@ -0,0 +1,37 @@
+// main app
+
+// this class provieds a structure for
+// tiket data
+class tiket_data{
+    public double charge = 0;
+    public String tiketType = "";
+    public String intializedTime = "";
+}
+
+// thiket machin class
+// this contains methods for tiket
+class machin_tiket{
+    public tiket_data tiketData = new tiket_data();
+
+    public machin_tiket(double firstCharge, String type){
+        tiketData.charge = firstCharge;
+        tiketData.tiketType = type;
+    }
+
+    public void showTiketData(){
+        System.out.println("Tiket Charge: " + tiketData.charge);
+        System.out.println("Tiket Type: " + tiketData.tiketType);
+    }
+
+}
+
+// main class
+public class main
+{
+    public static void main(String args[])
+    {
+     machin_tiket tiket = new machin_tiket(1200.000, "Pro");
+     tiket.showTiketData();
+    }
+}
+
