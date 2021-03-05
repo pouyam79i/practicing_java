18247852e44aaf1f6427a7c7560b5c20bb7f8700
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-03-05 16:04:22 +0330

    compex classes are created

--- src/Complex.java
+++ src/Complex.java
@@ -4,6 +4,28 @@ Uni ID: 9829039
 tel ID: @pouya_moh
  */
 
+class ComplexNumber{
+    public double Re;
+    public double Im;
+}
+
+class ComplexOpration{
+
+    public void plus(ComplexNumber complex_number1, ComplexNumber complex_number2){
+        ComplexNumber result = new ComplexNumber();
+        result.Re = complex_number1.Re + complex_number1.Re;
+        result.Im = complex_number1.Im + complex_number2.Im;
+        System.out.print(result.Re);
+        if(result.Im<0){
+            System.out.println(result.Im + "i");
+        }
+        else{
+            System.out.println("+" + result.Im + "i");
+        }
+    }
+
+}
+
 public class Complex {
 
 }
