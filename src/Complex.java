6e25d0bb86b826e20b6b342cc205b757ec8a8515
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-03-05 16:58:03 +0330

    classes are built but not debuged

--- src/Complex.java
+++ src/Complex.java
@@ -5,42 +5,66 @@ tel ID: @pouya_moh
  */
 
 import java.util.Scanner;
+import java.lang.Math;
 
 class ComplexNumber{
-    public double Re;
-    public double Im;
+    public int Re;
+    public int Im;
     Scanner scan = new Scanner(System.in);
 
     public void readComplex(){
-        Re = scan.nextDouble();
-        Im = scan.nextDouble();
+        Re = scan.nextInt();
+        Im = scan.nextInt();
     }
+
+    public void printComplex(){
+        System.out.print(Re);
+        if(Im<0){
+            System.out.println(Im + "i");
+        }
+        else{
+            if(Im != 1) {
+                System.out.println("+" + Im + "i");
+            }
+            else{
+                System.out.println("+" + "i");
+            }
+        }
+    }
+
 }
 
 class ComplexOpration{
 
     ComplexNumber result = new ComplexNumber();
 
-    private void printComplex(ComplexNumber complex_number){
-        System.out.print(complex_number.Re);
-        if(complex_number.Im<0){
-            System.out.println(complex_number.Im + "i");
-        }
-        else{
-            System.out.println("+" + complex_number.Im + "i");
-        }
-    }
 
     public void plus(ComplexNumber complex_number1, ComplexNumber complex_number2){
         result.Re = complex_number1.Re + complex_number2.Re;
         result.Im = complex_number1.Im + complex_number2.Im;
-        printComplex(result);
+        result.printComplex();
     }
 
     public void minus(ComplexNumber complex_number1, ComplexNumber complex_number2){
         result.Re = complex_number1.Re - complex_number2.Re;
         result.Im = complex_number1.Im - complex_number2.Im;
-        printComplex(result);
+        result.printComplex();
+    }
+
+    public void product(ComplexNumber complex_number1, ComplexNumber complex_number2){
+        result.Re = (complex_number1.Re * complex_number2.Re) - (complex_number1.Im * complex_number2.Im);
+        result.Im = (complex_number1.Re * complex_number2.Im) + (complex_number1.Im * complex_number2.Re);
+        result.printComplex();
+    }
+
+    public void divide(ComplexNumber complex_number1, ComplexNumber complex_number2){
+        result.Re = (complex_number1.Re * complex_number2.Re) + (complex_number1.Im * complex_number2.Im);
+        result.Im = (complex_number1.Im * complex_number2.Re) - (complex_number1.Re * complex_number2.Im) ;
+        int temp;
+        temp = (int)Math.sqrt(Math.pow(complex_number2.Re, 2) + Math.pow(complex_number2.Im, 2));
+        result.Re = result.Re / temp;
+        result.Im = result.Im / temp;
+        result.printComplex();
     }
 
 }
@@ -54,5 +78,7 @@ public class Complex {
         ComplexOpration complexOprator = new ComplexOpration();
         complexOprator.plus(CN1, CN2);
         complexOprator.minus(CN1, CN2);
+        complexOprator.product(CN1, CN2);
+        complexOprator.divide(CN1, CN2);
     }
 }
