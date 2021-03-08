63baa4b7e243a309a91eee914aadef5eafcef24f
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-03-08 17:31:47 +0330

    setter getter of Student class ... AP package ... created

--- src/APworkshop/Student.java
+++ src/APworkshop/Student.java
@@ -14,14 +14,43 @@ public class Student {
 
 
 // Setter methods:
-
     // Setter of First Name
     public void setFirstName(String input){
         firstName = input;
     }
+    // Setter of Last Name
+    public void setLasttName(String input){
+        lastName = input;
+    }
+    // Setter of University ID
+    public void setID(String input){
+        ID = input;
+    }
+    // Setter of Grade
+    public void setGrade(int input){
+        grade = input;
+    }
+
+
 
 // Getter methods:
-    public String getFirstName(){
+    // Getter of First Name
+    public String setFirstName(){
         return firstName;
     }
+    // Getter of Last Name
+    public String setLasttName(){
+        return lastName;
+    }
+    // Getter of University ID
+    public String setID(){
+        return ID;
+    }
+    // Getter of Grade
+    public int setGrade(){
+        return grade;
+    }
+
+
+
 }
