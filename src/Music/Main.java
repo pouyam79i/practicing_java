9136cf7306f3f749304506103b6633b03840fd04
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-03-24 02:20:56 +0430

    starting to rebuild main of music

--- src/Music/Main.java
+++ src/Music/Main.java
@@ -7,54 +7,58 @@ package Music;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        MusicCollection musicCollection = new MusicCollection();
-        Scanner scanner = new Scanner(System.in);
-        Menu menu = new Menu();
-        String input = "";
-        int index = -1;
-        System.out.println(menu.mainMenu);
 
-        label:
-        while(true){
-            input = scanner.nextLine();
-            switch (input) {
-                case "1":
-                    System.out.println(menu.playMenu);
-                    input = scanner.nextLine();
-                    index = Integer.parseInt(input);
-                    musicCollection.startPlaying(index);
-                    break;
-                case "2":
-                    System.out.println(menu.addMenu);
-                    input = scanner.nextLine();
-                    musicCollection.addFile(input);
-                    break;
-                case "3":
-                    System.out.println(menu.removeMenu);
-                    input = scanner.nextLine();
-                    index = Integer.parseInt(input);
-                    musicCollection.removeFile(index);
-                    break;
-                case "4":
-                    System.out.println(menu.listMenu);
-                    input = scanner.nextLine();
-                    input = input.toUpperCase();
-                    if (input.equals("ALL"))
-                        musicCollection.listAllFiles();
-                    else {
-                        index = Integer.parseInt(input);
-                        musicCollection.listFile(index);
-                    }
-                    break;
-                case "5":
-                    break label;
-                default:
-                    System.out.println("\nWrong Input\n");
-                    break;
-            }
-            System.out.println(menu.mainMenu);
-        }
     }
 }
+
+
+//    public static void main(String[] args) {
+//        MusicCollection musicCollection = new MusicCollection();
+//        Scanner scanner = new Scanner(System.in);
+//        Menu menu = new Menu();
+//        String input = "";
+//        int index = -1;
+//        System.out.println(menu.mainMenu);
+//
+//        label:
+//        while(true){
+//            input = scanner.nextLine();
+//            switch (input) {
+//                case "1":
+//                    System.out.println(menu.playMenu);
+//                    input = scanner.nextLine();
+//                    index = Integer.parseInt(input);
+//                    musicCollection.startPlaying(index);
+//                    break;
+//                case "2":
+//                    System.out.println(menu.addMenu);
+//                    input = scanner.nextLine();
+//                    musicCollection.addFile(input);
+//                    break;
+//                case "3":
+//                    System.out.println(menu.removeMenu);
+//                    input = scanner.nextLine();
+//                    index = Integer.parseInt(input);
+//                    musicCollection.removeFile(index);
+//                    break;
+//                case "4":
+//                    System.out.println(menu.listMenu);
+//                    input = scanner.nextLine();
+//                    input = input.toUpperCase();
+//                    if (input.equals("ALL"))
+//                        musicCollection.listAllFiles();
+//                    else {
+//                        index = Integer.parseInt(input);
+//                        musicCollection.listFile(index);
+//                    }
+//                    break;
+//                case "5":
+//                    break label;
+//                default:
+//                    System.out.println("\nWrong Input\n");
+//                    break;
+//            }
+//            System.out.println(menu.mainMenu);
+//        }
+//    }
--- src/Run.java
+++ src/Run.java
@@ -1,8 +1,14 @@
-
-
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Run{
     public static void main(String[] args) {
-        System.out.println("This is Me");
+        ArrayList<String> myArrList = new ArrayList<String>();
+        myArrList.add("Pouya is Good");
+        myArrList.add("Yes");
+        myArrList.add(1, "New Index");
+        for (String s : myArrList) {
+            System.out.println(s);
+        }
     }
 }
