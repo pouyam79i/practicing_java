663f46181c55b0be5dddc6b851dc8684188abca9
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-03-24 05:59:29 +0430

    adding favorite music abilities

--- src/Music/MusicCollection.java
+++ src/Music/MusicCollection.java
@@ -16,6 +16,8 @@ public class MusicCollection
     private ArrayList<Integer> times;
     // Storing artist name in artists.
     private ArrayList<String> artists;
+    // Storing favorite music
+    private ArrayList<Boolean> favorites;
     // A player for the music files.
     private MusicPlayer player;
     // Contains number of files.
@@ -26,11 +28,11 @@ public class MusicCollection
     /**
      * Create a Music.MusicCollection
      */
-    public MusicCollection()
-    {
+    public MusicCollection() {
         files = new ArrayList<String>();
         times = new ArrayList<Integer>();
         artists = new ArrayList<String>();
+        favorites = new ArrayList<Boolean>();
         player = new MusicPlayer();
         type = "Unclassified";
         numberOfFiles = 0;
@@ -39,11 +41,11 @@ public class MusicCollection
      * Create a Music.MusicCollection
      * @param Type is the type of collection
      */
-    public MusicCollection(String Type)
-    {
+    public MusicCollection(String Type) {
         files = new ArrayList<String>();
         times = new ArrayList<Integer>();
         artists = new ArrayList<String>();
+        favorites = new ArrayList<Boolean>();
         player = new MusicPlayer();
         type = Type;
         numberOfFiles = 0;
@@ -53,11 +55,11 @@ public class MusicCollection
      * Add a file to the collection.
      * @param filename The file to be added.
      */
-    public void addFile(String filename)
-    {
+    public void addFile(String filename) {
         files.add(filename);
         times.add(0);
         artists.add("Unknown");
+        favorites.add(false);
         numberOfFiles++;
     }
     /**
@@ -65,11 +67,11 @@ public class MusicCollection
      * @param filename The file to be added.
      * @param time the year of release
      */
-    public void addFile(String filename, int time)
-    {
+    public void addFile(String filename, int time) {
         files.add(filename);
         times.add(time);
         artists.add("Unknown");
+        favorites.add(false);
         numberOfFiles++;
     }
     /**
@@ -77,11 +79,11 @@ public class MusicCollection
      * @param filename The file to be added.
      * @param artist The artist name
      */
-    public void addFile(String filename, String artist)
-    {
+    public void addFile(String filename, String artist) {
         files.add(filename);
         times.add(0);
         artists.add(artist);
+        favorites.add(false);
         numberOfFiles++;
     }
     /**
@@ -90,11 +92,11 @@ public class MusicCollection
      * @param artist The artist name
      * @param time the year of release
      */
-    public void addFile(String filename, String artist, int time)
-    {
+    public void addFile(String filename, String artist, int time) {
         files.add(filename);
         times.add(time);
         artists.add(artist);
+        favorites.add(false);
         numberOfFiles++;
     }
 
@@ -103,8 +105,7 @@ public class MusicCollection
      * Return the number of files in the collection.
      * @return The number of files in the collection.
      */
-    public int getNumberOfFiles()
-    {
+    public int getNumberOfFiles() {
         numberOfFiles = files.size();            // checking to make sure :)
         return numberOfFiles;
     }
@@ -113,11 +114,16 @@ public class MusicCollection
      * List a file from the collection.
      * @param index The index of the file to be listed.
      */
-    public void listFile(int index)
-    {
+    public void listFile(int index) {
         if(validIndex(index)) {
-            System.out.println("Music Info: \n");
-            System.out.println((index + 1) + " - " + files.get(index) + "\n");
+            System.out.print((index) + " - " + files.get(index));
+            if(!artists.get(index).equals("Unknown")) {
+                System.out.print(" - artist: " + artists.get(index));
+            }
+            if(times.get(index) != 0) {
+                System.out.print(" - release date: " + times.get(index));
+            }
+            System.out.println();
         }
         else
             System.out.println("Wrong Index\n");
@@ -126,12 +132,11 @@ public class MusicCollection
     /**
      * Show a list of all the files in the collection.
      */
-    public void listAllFiles()
-    {
+    public void listAllFiles() {
         if(files.size() > 0){
             System.out.println("\nAll musics, total number: " + files.size());
             for (int i = 0; i < files.size(); i++) {
-                System.out.print((i + 1) + " - " + files.get(i));
+                System.out.print(i + " - " + files.get(i));
                 if(!artists.get(i).equals("Unknown")) {
                     System.out.print(" - artist: " + artists.get(i));
                 }
@@ -149,8 +154,7 @@ public class MusicCollection
      * Remove a file from the collection.
      * @param index The index of the file to be removed.
      */
-    public void removeFile(int index)
-    {
+    public void removeFile(int index) {
         if(validIndex(index)) {
             files.remove(index);
             times.remove(index);
@@ -161,12 +165,42 @@ public class MusicCollection
     }
 
     /**
+     * set the music we want as favorite
+     * @param index to true
+     */
+    public void setFavorite(int index) {
+        if(validIndex(index))
+            favorites.set(index, true);
+    }
+
+    /**
+     * remove the music we want from favorite
+     * @param index to true
+     */
+    public void removeFavorite(int index) {
+        if(validIndex(index))
+            favorites.set(index, false);
+    }
+
+    /**
+     * prints All favorite music information
+     */
+    public void listFavorite(){
+        int i = 0;
+        for(i = 0; i < files.size();i++){
+            if(favorites.get(i))
+                listFile(i);
+        }
+        if(i == 0)
+            System.out.println("No favorite music");
+    }
+
+    /**
      * Start playing a file in the collection.
      * Use stopPlaying() to stop it playing.
      * @param index The index of the file to be played.
      */
-    public void startPlaying(int index)
-    {
+    public void startPlaying(int index) {
         if(validIndex(index))
             player.startPlaying(files.get(index));
         else
@@ -183,11 +217,9 @@ public class MusicCollection
     /**
      * Stop the player.
      */
-    public void stopPlaying()
-    {
+    public void stopPlaying(){
         player.stop();
     }
-
 
     /**
      * Determine whether the given index is valid for the collection.
@@ -195,8 +227,7 @@ public class MusicCollection
      * @param index The index to be checked.
      * @return true if the index is valid, false otherwise.
      */
-    private boolean validIndex(int index)
-    {
+    private boolean validIndex(int index) {
         int tempSize = files.size();
         return (index >= 0 && index < tempSize);
     }
