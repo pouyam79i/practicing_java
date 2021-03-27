0b42a114a94ca0981786ae14d9cbc51cd0c97494
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-03-27 17:45:27 +0430

    main and menu file created

---
+++ src/APworkshop/Main.java
@@ -0,0 +1,18 @@
+package APworkshop;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Lab> labs = new ArrayList<Lab>();
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOfLabs = 0;
+        String input = "";
+
+        while (true){
+
+        }
+    }
+}
---
+++ src/APworkshop/Menu.java
@@ -0,0 +1,19 @@
+/*
+Coded by Pouya Mohammadi
+CE@AUT - uni ID: 9829039
+ */
+package APworkshop;
+
+public class Menu {
+
+    // Main menu massage
+    public final String mainMenu = """
+            Main Menu
+            1 - Add a class
+            2 - Edit a class
+            3 - List all classes
+            4 - Remove a class
+            5 - Exit
+            """;
+
+}
