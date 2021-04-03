7c2c8ffaf6ba504d7527a9660ebef96f49ecac5b
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-04-04 01:59:51 +0430

    updating and debuging

--- src/myLinkedList/Main.java
+++ src/myLinkedList/Main.java
@@ -1,4 +1,16 @@
 package myLinkedList;
 
 public class Main {
+    public static void main(String[] args) {
+        MyLinkedList intList = new MyLinkedList();
+        intList.add(13);
+        intList.add(6);
+        intList.add(25);
+        intList.print();
+        System.out.println(intList.getSize());
+        intList.removeMiddle();
+        intList.print();
+        intList.removeIndex(0);
+        intList.print();
+    }
 }
--- src/myLinkedList/MyLinkedList.java
+++ src/myLinkedList/MyLinkedList.java
@@ -1,25 +1,43 @@
 package myLinkedList;
 
+/**
+ * Class of node
+ */
 class Node{
 
+    // next node
     private Node next;
+    // previous node
     private Node previous;
-
+    // data of this node
     private int data;
 
+    /**
+     * set next and previous to null
+     */
     public Node(){
         next = null;
         previous = null;
     }
+    /**
+     * next node will be set to null
+     * @param previous will be set as previous node
+     */
     public Node(Node previous){
         next = null;
         this.previous = previous;
     }
+
+    /**
+     * @param next will be set as next node
+     * @param previous will be set as previous node
+     */
     public Node(Node next, Node previous){
         this.next = next;
         this.previous = previous;
     }
 
+    // Getters
     public Node getNext() {
         return next;
     }
@@ -30,7 +48,7 @@ class Node{
         return data;
     }
 
-
+    // Setters
     public void setNext(Node next) {
         this.next = next;
     }
@@ -43,30 +61,51 @@ class Node{
 
 }
 
+/**
+ * class of linked list
+ */
 public class MyLinkedList {
 
+    // head of list
     private Node head;
+    // size of list
     private int size;
 
+    /**
+     * Constructor
+     */
     public MyLinkedList(){
         head = null;
         size = 0;
     }
 
+    /**
+     * add to the end of list
+     * @param number will be added
+     */
     public void add(int number){
         Node newNode = new Node(null);
         newNode.setData(number);
         if(head == null)
             head = newNode;
         else{
+            if(size == 1) {
+                newNode.setPrevious(head);
+                head.setNext(newNode);
+            }
             Node temp = head;
             for (int i = 1; i < size; i++)
                 temp = temp.getNext();
+            newNode.setPrevious(temp);
             temp.setNext(newNode);
         }
         size++;
     }
 
+    /**
+     * adds to the head of list
+     * @param number will be added to the head of list
+     */
     public void addFirst(int number){
         Node newNode = new Node(head, null);
         newNode.setData(number);
@@ -74,6 +113,10 @@ public class MyLinkedList {
         size++;
     }
 
+    /**
+     * Print the middle data
+     * if it was empty, -1 will be printed
+     */
     public void findMiddle(){
         boolean isFound = false;
         int index = (size+1) / 2;
@@ -87,30 +130,44 @@ public class MyLinkedList {
             }
             temp = temp.getNext();
         }
-        if(!isFound) {
-            System.out.println(-1);
-        }
     }
 
+    /**
+     * Remove the middle data
+     */
     void removeMiddle(){
         int index = (size+1) / 2;
         index--;
+        if(size == 0){
+            System.out.println();
+            return;
+        }
         if(index == 0){
             head = head.getNext();
+            if(head != null)
+                head.setPrevious(null);
             size--;
             return;
         }
         Node temp = head;
         for(int i = 0; i < size; i++){
+            System.out.println("Flag: " + i);
             if(i == index){
                 temp.getPrevious().setNext(temp.getNext());
+                temp.getNext().setPrevious(temp.getPrevious());
                 break;
             }
+            if(temp == null)
+                break;
             temp = temp.getNext();
         }
+        if(size > 0)
+            size--;
     }
 
-
+    /**
+     * will print all nodes' data
+     */
     void print(){
         Node temp = head;
         if(head == null){
@@ -123,7 +180,12 @@ public class MyLinkedList {
         }
     }
 
+    /**
+     * Search if it exists in the list
+     * @param number will be searched
+     */
     void contains(int number){
+        boolean isFound = false;
         Node temp = head;
         int index = 0;
         if(head == null){
@@ -132,16 +194,26 @@ public class MyLinkedList {
         }
         while (temp != null){
             if (temp.getData() == number) {
+                isFound = true;
                 System.out.println(index);
                 break;
             }
             temp = temp.getNext();
         }
+        if(!isFound)
+            System.out.println(-1);
     }
 
+    /**
+     * Remove a node with specified index
+     * @param index will be search and the according node of this index  will be removed
+     */
     void removeIndex(int index){
-        if(index == 0){
-            head = head.getNext();
+        if(index == 0 && size > 0){
+            if(head != null)
+                head = head.getNext();
+            if(head != null)
+                head.setPrevious(null);
             if(size > 0)
                 size--;
             return;
@@ -150,9 +222,20 @@ public class MyLinkedList {
         for(int i = 0; i < size; i++){
             if(i == index){
                 temp.getPrevious().setNext(temp.getNext());
+                temp.getNext().setPrevious(temp.getPrevious());
+                size--;
                 break;
             }
+            if(temp == null)
+                break;
+            temp = temp.getNext();
         }
     }
 
+    // Getters
+    public int getSize() {
+        return size;
+    }
+
 }
+
